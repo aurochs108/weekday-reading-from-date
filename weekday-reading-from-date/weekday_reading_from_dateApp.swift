8de6ea44700fc00940dff98867a02b04bcef4a22
Dawid <Zubrowski>
2024-05-29 23:12:06 +0200

    Initial Commit

---
+++ weekday-reading-from-date/weekday_reading_from_dateApp.swift
@@ -0,0 +1,17 @@
+//
+//  weekday_reading_from_dateApp.swift
+//  weekday-reading-from-date
+//
+//  Created by Dawid on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct weekday_reading_from_dateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
